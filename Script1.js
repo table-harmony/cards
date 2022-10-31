4844786f2d967ee94fe60ed47231c0413e170429
liron1111 <114408962+liron1111@users.noreply.github.com>
2022-10-31 23:45:13 +0200

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,45 @@
+let activeIndex = 0;
+
+const groups = document.getElementsByClassName("card-group");
+
+function handleRightButton() {
+    const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+    const currentCardGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextCardGroup = document.querySelector(`[data-index="${nextIndex}"]`),
+        currentTextGroup = document.querySelector(`[text-index="${activeIndex}"]`),
+        nextTextGroup = document.querySelector(`[text-index="${nextIndex}"]`);
+    
+    currentCardGroup.dataset.status = "after";
+    currentTextGroup.dataset.status = "after";
+
+    nextCardGroup.dataset.status = "becoming-active-from-before";
+    nextTextGroup.dataset.status = "becoming-active-from-before";
+
+    nextTextGroup.dataset.status = "active";
+    setTimeout(() => {
+        nextCardGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
+
+function handleLeftButton() {
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+    const currentCardGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextCardGroup = document.querySelector(`[data-index="${nextIndex}"]`),
+        currentTextGroup = document.querySelector(`[text-index="${activeIndex}"]`),
+        nextTextGroup = document.querySelector(`[text-index="${nextIndex}"]`);
+
+    currentCardGroup.dataset.status = "before";
+    currentTextGroup.dataset.status = "before";
+
+    nextCardGroup.dataset.status = "becoming-active-from-after";
+    nextTextGroup.dataset.status = "becoming-active-from-after";
+
+    setTimeout(() => {
+        nextCardGroup.dataset.status = "active";
+        nextTextGroup.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
